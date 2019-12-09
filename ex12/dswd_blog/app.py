106b292b2daffd559664d3df35f8bea4058adf09
me <psdonium@gmail.com>
2019-12-10 07:26:43 +0800

    initial ex12

---
+++ ex12/dswd_blog/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from . import auth, public, config
+from .extensions import db, migrate, login_manager, csrf_protect
+from . import models
+
+
+def create_app(testing=False):
+    app = Flask(__name__)
+
+    if testing:
+        app.config.from_object(config.TestingConfig)
+    else:
+        app.config.from_object(config.DevConfig)
+
+    register_blueprints(app)
+    register_extensions(app)
+
+    return app
+
+
+def register_blueprints(app):
+    app.register_blueprint(auth.views.bp)
+    app.register_blueprint(public.views.bp)
+
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate.init_app(app, db)
+    csrf_protect.init_app(app)
+    login_manager.init_app(app)
